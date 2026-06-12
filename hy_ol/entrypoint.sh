6b154a7335a7b5090b016225342b058c58128ce3
Mackerly <duan0731@outlook.com>
2026-06-12 10:48:36 +0800

    Update entrypoint.sh

--- hy_ol/entrypoint.sh
+++ hy_ol/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 envsubst "\${PORT} \${PASSWORD}" < /ser.yml.template > /data/hysteria2/server.yml
 curl -s ip.sb
-openssl x509 -fingerprint -sha256 -noout -in /hy2.crt
+openssl x509 -fingerprint -sha256 -noout -in /hy2.crt | awk -F'=' '{print "pinSHA256=" $2 }'
 exec "$@"
